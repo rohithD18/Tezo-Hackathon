edc38f8ae097e86b0dc32c538001612d868a70a6
SumitRaj0 <sumitrajoffical@gmail.com>
2024-03-15 18:16:58 +0530

    Application screen, All teams Screen, Profile view implementation

---
+++ src/components/AllTeams.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "../styles/AllTeams.css";
+import PaginationSection from "./pagination/PaginationSection";
+import TeamCard from "./TeamCard";
+import { TeamsData } from "../services/Const";
+
+const AllTeams: React.FC = () => {
+  const [currentItem, setCurrentItem] = useState<any[]>([]);
+
+  return (
+    <>
+      <div className="allCardBody">
+        {currentItem.map((item) => (
+          <TeamCard key={item.id} data={item} />
+        ))}
+      </div>
+      <div className="Bodyofpagination">
+        <PaginationSection setCurrentItem={setCurrentItem} data={TeamsData} />
+      </div>
+    </>
+  );
+};
+
+export default AllTeams;
--- src/components/SubNavBar.tsx
+++ src/components/SubNavBar.tsx
@@ -1,27 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../styles/MyTeam.css";
-const SubNavBar = () => {
-  const [activeItem, setActiveItem] = useState<null|string>();
-  const handleItemClick = (itemName:string) => {
+import { type } from "os";
+
+type Props = {
+  setShowAll: any;
+};
+
+const SubNavBar = (props: Props) => {
+  const { setShowAll } = props;
+  const [activeItem, setActiveItem] = useState<null | string>();
+  const handleItemClick = (itemName: string) => {
     setActiveItem(itemName);
+    {
+      itemName === "All" ? setShowAll(true) : setShowAll(false);
+    }
   };
   return (
     <>
-    <div className="row topContainer">
-    <div className={`col-sm-1 item ${activeItem === 'All' ? 'active' : ''}`} onClick={() => handleItemClick('All')} >
-     <label className={`${activeItem === 'All' ? 'active' : ''}`} onClick={() => handleItemClick('All')}> All</label>
-    </div>
-    <div className={`col-sm-1 item ${activeItem === 'My Team' ? 'active' : ''}`} onClick={() => handleItemClick('My Team')}  style={{width:110}}>
- 
-         <label className={`${activeItem === 'My Team' ? 'active' : ''}`} onClick={() => handleItemClick('My Team')} > My Team</label> 
-    
-    </div>
-  </div>
-<div className="borderStyling">
+      <div className="row topContainer">
+        <div
+          className={`col-sm-1 item ${activeItem === "All" ? "active" : ""}`}
+          onClick={() => handleItemClick("All")}
+        >
+          <label
+            className={`${activeItem === "All" ? "active" : ""}`}
+            onClick={() => handleItemClick("All")}
+          >
+            {" "}
+            All
+          </label>
+        </div>
+        <div
+          className={`col-sm-1 item ${
+            activeItem === "My Team" ? "active" : ""
+          }`}
+          onClick={() => handleItemClick("My Team")}
+          style={{ width: 110 }}
+        >
+          <label
+            className={`${activeItem === "My Team" ? "active" : ""}`}
+            onClick={() => handleItemClick("My Team")}
+          >
+            {" "}
+            My Team
+          </label>
+        </div>
+      </div>
+      <div className="borderStyling"></div>
+    </>
+  );
+};
 
-</div>
-</>
-  )
-}
-
-export default SubNavBar
+export default SubNavBar;
