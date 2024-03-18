5dbec37798ee61ae316c3c644372b51f8ec36484
Likitha-Kattamudi <kattamudilikitha@gmail.com>
2024-03-18 13:37:34 +0530

    HomePage After Registration (Timer Component)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { teamMembersArray } from "./TeamMembers";
 import { ITeamMembers } from "../Interfaces";
 import mainBG from "../assets/mainBG.png";
+import timerIcon from "../assets/timerIcon.png";
 interface HomeProps {
   isRegister: boolean;
 }
@@ -16,17 +17,20 @@ const Home: React.FC<HomeProps> = (props) => {
   const [description,setDescription] =useState<string>("");
   const [buttonValue,setButtonValue]=useState<string>("");
   const [flag,setFlag]=useState<boolean>();
+  const [date,setDate]=useState<string>("");
   useEffect(() => {
     if(props.isRegister){
       setTitle("Welcome to Tezo Hackathon!");
       setDescription("You’re registered for this hackathon");
       setButtonValue("Enter a submission")
+      setDate("Dec 10, 2023");
       setFlag(false);
     }
     else{
       setTitle("Secure Your Spot at the Forefront! 🚀");
       setDescription("Be among the first five teams to REGISTER and SUBMIT your project topics for an exclusive REVIEW by our esteemed judges. Stand out, get noticed, and pave your way to a VIP ONE-ON-ONE session");
-      setButtonValue("Register Now")
+      setButtonValue("Register Now");
+      setFlag(true);
     }
   
   },[props.isRegister]);
@@ -36,7 +40,7 @@ const Home: React.FC<HomeProps> = (props) => {
         <div className="introDiv">
           <div id="backgroundImage1" >
           <div   className="left">
-            {flag:<div className="members">             
+            {flag?(<div className="members">             
             
             <div className="profileContainer">
             {visibleImages.map((item, index) => (
@@ -45,15 +49,20 @@ const Home: React.FC<HomeProps> = (props) => {
              
               )
               }
-              </div>? <div></div>}
+              </div>
              <label className="noOfMembers"> + {members.length} participating</label>
-            </div>
+            </div>):<div className="timeContainer">
+              <div> <img src={timerIcon} alt="timer icon" width={32} height={32}></img>
+                
+                <label id="time">{date}</label></div>
+              <div id="timer">14 more days to submit</div>
+              </div>}
           <h1 className="header">
           {title}
           </h1>
           <p className="description">{description}</p>
           <button
-            onClick={() => navigate("/Registraion-Form")}
+            onClick={() => navigate("/registration-form")}
             id="registerBtn"
           >
             {buttonValue}
