ed979eae338c4b23b2deff08a067924bb5ed99d0
swathi-gorli <hymavathiswathigorli2222@gmail.com>
2024-03-20 15:14:24 +0530

    dashboardview screen

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import AdminNavBar from "./DashboardNav";
 import Application from "./Application";
+import DashboardView from "./DashboardView";
 
-const Dashboard = () => {
+const Dashboard:React.FC = () => {
   const [isApplication, setIsApplication] = useState(false);
   return (
     <div className="adminHomeDiv">
       <div>
         <AdminNavBar setIsApplication={setIsApplication} />
       </div>
+      <div style={{width:"100%"}}>{!isApplication && <DashboardView />}</div>
       <div>{isApplication && <Application />}</div>
     </div>
   );
